a80c197ea8e4fd787a7a2df706f1ad02cc43cc2f
lirance <lirance@users.noreply.github.com>
2019-11-07 20:14:51 -0800

    change framework

--- config.js
+++
@@ -1,7 +0,0 @@
-const { argv } = require('yargs');
-
-exports.MONGODB = {
-    uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/node`,
-    username: argv.db_username || 'DB_username',
-    password: argv.db_password || 'DB_password',
-};
